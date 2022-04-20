bcf56994b8bcbd28beea824749df90c19b852225
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-04-20 12:22:34 +0200

    ...

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -34,211 +34,6 @@
 	</nav>
 </header>
 
-<!-- <script>
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
-	import { onMount } from 'svelte';
-
-	// Show mobile icon and display menu
-	let showMobileMenu = false;
-
-	// List of navigation items
-	const navItems = [{ label: 'logo', href: '#' }];
-
-	// Mobile menu click event handler
-	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
-
-	// Media match query handler
-	const mediaQueryHandler = (each) => {
-		// Reset mobile state
-		if (!each.matches) {
-			showMobileMenu = false;
-		}
-	};
-
-	// Attach media query listener on mount hook
-	onMount(() => {
-		const mediaListener = window.matchMedia('(max-width: 767px)');
-
-		mediaListener.addListener(mediaQueryHandler);
-	});
-</script>
-
-<header>
-	<nav>
-		<div class="inner">
-			<div on:click={handleMobileIconClick} class={`mobile-icon${showMobileMenu ? ' active' : ''}`}>
-				<div class="middle-line" />
-			</div>
-			<ul class={`navbar-list${showMobileMenu ? ' mobile' : ''}`}>
-				{#each navItems as item}
-					<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-					<li class:active={$page.url.pathname === '/about'}>
-						<a sveltekit:prefetch href="/om_os">Om os</a>
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</nav>
-	<div class="corner">
-		<a href="/">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-</header>
-
-<style>
-	header {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		background-color: rgba(0, 0, 0, 0.8);
-	}
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-		position: static;
-		z-index: 3;
-	}
-
-	.inner {
-		padding-left: 20px;
-		padding-right: 20px;
-		margin: auto;
-		box-sizing: border-box;
-		display: flex;
-		height: 100%;
-	}
-
-	.mobile-icon {
-		width: 25px;
-		height: 14px;
-		position: relative;
-		cursor: pointer;
-	}
-
-	.mobile-icon:after,
-	.mobile-icon:before,
-	.middle-line {
-		content: '';
-		position: absolute;
-		width: 100%;
-		height: 2px;
-		background-color: #fff;
-		transition: all 0.4s;
-		transform-origin: center;
-	}
-
-	.mobile-icon:before,
-	.middle-line {
-		top: 0;
-	}
-
-	.mobile-icon:after,
-	.middle-line {
-		bottom: 0;
-	}
-
-	.mobile-icon:before {
-		width: 66%;
-	}
-
-	.mobile-icon:after {
-		width: 33%;
-	}
-
-	.middle-line {
-		margin: auto;
-	}
-
-	.mobile-icon:hover:before,
-	.mobile-icon:hover:after,
-	.mobile-icon.active:before,
-	.mobile-icon.active:after,
-	.mobile-icon.active .middle-line {
-		width: 100%;
-	}
-
-	.mobile-icon.active:before,
-	.mobile-icon.active:after {
-		top: 50%;
-		transform: rotate(-45deg);
-	}
-
-	.mobile-icon.active .middle-line {
-		transform: rotate(45deg);
-	}
-
-	.navbar-list {
-		display: none;
-		width: 100%;
-		justify-content: space-between;
-		margin: 0;
-		padding: 0 40px;
-	}
-
-	.navbar-list.mobile {
-		background-color: rgba(0, 0, 0, 0.8);
-		position: fixed;
-		display: block;
-		height: calc(100% - 45px);
-		bottom: 0;
-		left: 0;
-	}
-
-	.navbar-list li {
-		list-style-type: none;
-		position: relative;
-	}
-
-	.navbar-list li:before {
-		content: '';
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 100%;
-		height: 1px;
-	}
-
-	.navbar-list a {
-		color: #fff;
-		text-decoration: none;
-		display: flex;
-		height: 45px;
-		align-items: center;
-		padding: 0 10px;
-		font-size: 13px;
-	}
-
-	@media only screen and (min-width: 767px) {
-		.mobile-icon {
-			display: none;
-		}
-
-		.navbar-list {
-			display: flex;
-			padding: 0;
-		}
-
-		.navbar-list a {
-			display: inline-flex;
-		}
-	}
-</style> -->
 <style>
 	.corner {
 		width: 3em;
@@ -329,7 +124,7 @@
 	.menu__btn {
 		display: flex;
 		align-items: center;
-		position: fixed;
+		position: absolute;
 		top: 10px;
 		right: 20px;
 
@@ -366,12 +161,12 @@
 	.menu__box {
 		display: flex;
 		flex-direction: column;
-		position: absolute;
+		position: fixed;
 		visibility: hidden;
 		top: 0;
 		right: -100%;
 
-		width: 50%;
+		width: 100%;
 		height: 100%;
 
 		margin: 0;
