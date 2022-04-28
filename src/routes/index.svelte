67a3f0a6e02fe5a4586c0e74ecafbacb37a5720d
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-04-28 12:22:31 +0200

    bg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -50,7 +50,7 @@
 <style>
 	#hero {
 		background-image: url('../../static/hero_bg_2.svg');
-		background-image: url('hero_bg_2.svg');
+		/* background-image: url('hero_bg_2.svg'); */
 		width: 100%;
 		background-repeat: no-repeat;
 		background-size: cover;
